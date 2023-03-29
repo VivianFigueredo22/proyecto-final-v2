2b0e94eb5aa8e7f6600d841b0b802b648622b6ee
Vivian Figuereo <125564386+VivianFigueredo22@users.noreply.github.com>
2023-03-29 14:51:15 -0300

    mi primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogosComponent } from './components/logos/logos.component';
+import { SocialComponent } from './components/social/social.component';
+import {BannerComponent} from './components/banner/banner.component' ;
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    HeaderComponent,
+    LogosComponent,
+    SocialComponent,
+    BannerComponent
+
+    ],
   imports: [
     BrowserModule,
     AppRoutingModule
