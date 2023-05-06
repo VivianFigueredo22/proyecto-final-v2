1e609b3b5ccfa4c0dd4b4ef21343929d19338252
Vivian Figuereo <125564386+VivianFigueredo22@users.noreply.github.com>
2023-05-06 11:12:28 -0300

    ultimo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,25 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogosComponent } from './components/logoAP/logos.component';
-import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { SobreMiComponent } from './components/sobre-mi/sobre-mi.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 import { EducacionComponent } from './components/educacion/educacion.component' ;
 import { HabilidadesComponent } from './components/habilidades/habilidades.component';
-import  {  NgCircleProgressModule  }  from  'ng-circle-progress' ; 
+import  {  NgCircleProgressModule  }  from  'ng-circle-progress';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { FooterComponent } from './components/footer/footer.component' ; 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LogosComponent,
-    SocialComponent,
     BannerComponent,
     SobreMiComponent,
     ExperienciaComponent,
     EducacionComponent,
-    HabilidadesComponent
+    HabilidadesComponent,
+    ProyectosComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
